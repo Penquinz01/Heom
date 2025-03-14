8d5dd1397d96975aa7aeaf8774215f0a379c4cad
Janbaas Jamal K K <janbaasjamal1@gmail.com>
2025-03-14 19:34:01 +0530

    player input

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,47 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CharacterController2D))]
+public class PlayerInput : MonoBehaviour
+{
+    private Player _input;
+    public Vector2 Movement { get; private set; }
+    public event Action Attack;
+    public event Action Jump;
+
+    private void Awake()
+    {
+        _input = new Player();
+        _input.Enable();
+        _input.PlayerActions.Movement.performed += MovePerformed;
+        _input.PlayerActions.Attack.performed += AttackPerformed;
+        _input.PlayerActions.Jump.performed += JumpPerformed;
+    }
+
+    private void JumpPerformed(InputAction.CallbackContext context)
+    {
+        Attack?.Invoke();
+    }
+
+    private void AttackPerformed(InputAction.CallbackContext context)
+    {
+        Jump?.Invoke();
+    }
+
+    private void MovePerformed(InputAction.CallbackContext context)
+    {
+        Movement = context.ReadValue<Vector2>();
+    }
+
+    private void OnDisable()
+    {
+        _input.Disable();
+    }
+    private void OnEnable()
+    {
+        _input.Enable();
+    }
+   
+}
