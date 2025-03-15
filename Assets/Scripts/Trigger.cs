1e0dac4a02029d51eb594c7d70efb36ca735bb33
Janbaas Jamal K K <janbaasjamal1@gmail.com>
2025-03-15 10:56:55 +0530

    input fixed

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    [SerializeField] private int id;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(TriggerEvent());
+        }
+    }
+    IEnumerator TriggerEvent()
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameManager.Instance.Opengates(id);
+    }
+}
