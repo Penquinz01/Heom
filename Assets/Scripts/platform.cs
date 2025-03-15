916cbeb65b171340a53dde26c105ceb9f27d7d12
Antony Sajan <antonysajant@gmail.com>
2025-03-15 13:59:19 +0530

    Update platform.cs

--- Assets/Scripts/platform.cs
+++ Assets/Scripts/platform.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class platform : MonoBehaviour {
+=======
+public class platform: MonoBehaviour
+{
+>>>>>>> Stashed changes
     [SerializeField] private float y1;
     [SerializeField] private float y2;
-    [SerializeField] private Rigidbody2D rb;
     [SerializeField] private float speed;
+<<<<<<< Updated upstream
     bool up = true;
     void Start() {
         rb.linearVelocityX = speed;
@@ -18,6 +23,35 @@ public class platform : MonoBehaviour {
         if (transform.position.y < y2) {
             rb.linearVelocityX = 0;
             rb.linearVelocityX = speed;
+=======
+
+    private bool up = true;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (up)
+        {
+            if (transform.position.y < y1)
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+            else
+                up = false;
         }
+        else
+        {
+            if (transform.position.y > y2)
+                transform.Translate(Vector2.down * speed * Time.deltaTime);
+            else
+                up = true;
+>>>>>>> Stashed changes
+        }
+            
     }
 }
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
